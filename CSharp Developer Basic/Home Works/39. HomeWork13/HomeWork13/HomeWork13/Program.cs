98c697a8aa0488752cd1290c67d4e8a7dabaf4c6
proninp <proninpaul@yandex.ru>
2023-09-16 12:50:55 +0300

    OTUS C# Basic HomeWork13

---
+++ CSharp Developer Basic/Home Works/39. HomeWork13/HomeWork13/HomeWork13/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HomeWork13
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var listPart = list.Top(30);
+            Console.WriteLine(string.Join(", ", listPart));
+
+            var persons = new List<Person>()
+            {
+
+                new Person("Bjarne", 72),
+                new Person("James", 68),
+                new Person("Bill", 67),
+                new Person("Linus", 53),
+                new Person("Guido", 67),
+                new Person("Brendan", 62)
+            };
+            var personsPart = persons.Top(50, p => p.Age);
+            Console.WriteLine(string.Join(", ", personsPart));
+        }
+    }
+}
---
+++ CSharp Developer Basic/Home Works/39. HomeWork13/HomeWork13/HomeWork13/TopExtension.cs
@@ -0,0 +1,28 @@
+﻿namespace HomeWork13
+{
+    public static class TopExtension
+    {
+        public static IEnumerable<T> Top<T>(this IEnumerable<T> collection, int percent)
+        {
+            CheckPercentage(percent);
+            var elementsCount = GetElementsCount(collection, percent);
+            return collection.OrderByDescending(x => x).Take(elementsCount);
+        }
+        public static IEnumerable<T> Top<T>(this IEnumerable<T> collection, int percent, Func<T, int> predicate)
+        {
+            CheckPercentage(percent);
+            var elementsCount = GetElementsCount(collection, percent);
+            return collection.OrderByDescending(predicate).Take(elementsCount);
+        }
+
+        private static void CheckPercentage(int percent)
+        {
+            if (percent is < 1 or > 100)
+                throw new ArgumentException("Параметр должен быть в диапазоне от 1 до 100.");
+        }
+        
+        private static int GetElementsCount<T>(IEnumerable<T> collection, int percent) =>
+            (int)Math.Round(collection.Count() / 100.0 * percent, MidpointRounding.AwayFromZero);
+
+    }
+}
