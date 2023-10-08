95731891c2253a5701366218498fb271d1af7c19
proninp <proninpaul@yandex.ru>
2023-10-08 11:03:29 +0300

    OTUS C# Basic Prim's Algorithm - Added Pretty Print + Readme

--- CSharp Developer Basic/Home Works/47. Prims Algorithm/PrimsAlgorithm/PrimsAlgorithm/Program.cs
+++ CSharp Developer Basic/Home Works/47. Prims Algorithm/PrimsAlgorithm/PrimsAlgorithm/Program.cs
@@ -13,19 +13,19 @@
                 new int[] { 7, 0 } 
             };
             var minDistancePoints = MinDistanceConnectPoints(points);
-            Console.WriteLine(string.Join(" ", minDistancePoints.Select(p => $"[{p[0]}, {p[1]}]")));
+            Console.WriteLine(string.Join("", minDistancePoints.Skip(1).Select(p => $"[{p.Item1[0]}, {p.Item1[1]}] -> [{p.Item2[0]}, {p.Item2[1]}]\n")));
         }
-        public static List<int[]> MinDistanceConnectPoints(int[][] points)
+        public static List<(int[], int[])> MinDistanceConnectPoints(int[][] points)
         {
             var n = points.Length;
-            var dictionary = new Dictionary<int, List<Tuple<int, int>>>();
-            var output = new List<int[]>(n);
+            var dictionary = new Dictionary<int, List<Tuple<int, Tuple<int, int>>>>();
+            var output = new List<(int[], int[])>(n);
 
             if (n == 1)
-                return new List<int[]>() { points[0] };
+                return new List<(int[], int[])>() { (points[0], points[0]) };
 
             for (var i = 0; i < n; i++)
-                dictionary.Add(i, new List<Tuple<int, int>>()); // В Tuple - расстояние, индекс
+                dictionary.Add(i, new List<Tuple<int, Tuple<int, int>>>()); // В Tuple - расстояние, индекс
 
             // Для каждой точки собираем смежные точки и считаем расстояние
             for (var i = 0; i < n; i++)
@@ -40,28 +40,32 @@
 
                     var dist = Math.Abs(x2 - x1) + Math.Abs(y1 - y2);
 
-                    dictionary[j].Add(new Tuple<int, int>(dist, i));
-                    dictionary[i].Add(new Tuple<int, int>(dist, j));
+                    dictionary[j].Add(Tuple.Create(dist, Tuple.Create(i, j)));
+                    dictionary[i].Add(Tuple.Create(dist, Tuple.Create(j, i)));
                 }
             }
 
             // Алгоритм Прима
             var visited = new HashSet<int>();
-            var minHeap = new PriorityQueue<(int, int), int>();
-            minHeap.Enqueue((0, 0), 0);
+            var minHeap = new PriorityQueue<(int, Tuple<int, int>), int>();
+            var prevPoint = 0;
+            minHeap.Enqueue((0, Tuple.Create(0, prevPoint)), 0);
             
             while (minHeap.Count > 0)
             {
-                var (dist, point) = minHeap.Dequeue();
-                if (visited.Contains(point))
+                var pointsPair = minHeap.Dequeue().Item2;
+                if (visited.Contains(pointsPair.Item1))
                     continue;
-                output.Add(points[point]);
-                visited.Add(point);
-                var adj = dictionary[point];
+                prevPoint = pointsPair.Item2;
+                output.Add((points[prevPoint], points[pointsPair.Item1]));
+                visited.Add(pointsPair.Item1);
+                if (visited.Count == n)
+                    break;
+                var adj = dictionary[pointsPair.Item1];
 
                 for (var i = 0; i < adj.Count; i++)
                 {
-                    if (!visited.Contains(adj[i].Item2))
+                    if (!visited.Contains(adj[i].Item2.Item1))
                         minHeap.Enqueue((adj[i].Item1, adj[i].Item2), adj[i].Item1);
                 }
             }
